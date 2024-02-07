f99f532182ce1c7f16e3ba60be303ded155972ee
Scott Schmitz <sschmitz@michiganlabs.com>
2024-02-07 12:05:52 -0500

    Improve tests

--- networking/src/main/java/com/michiganlabs/hoverboard/data/Moshi.kt
+++ networking/src/main/java/com/michiganlabs/hoverboard/data/Moshi.kt
@@ -1,20 +1,44 @@
 package com.michiganlabs.hoverboard.data
 
+import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import java.lang.reflect.Type
 
+/**
+ * Write the provided object to a JSON string
+ *
+ * @param obj to convert to JSON string
+ * @return the JSON formatted String
+ */
 inline fun <reified T : Any> Moshi.dump(obj: T): String {
     return this.adapter(T::class.java).toJson(obj)
 }
 
+/**
+ * Load an object of type T from a JSON String.
+ * Example usage: moshi.load<Foo>(json)
+ *
+ * @param json the json string
+ * @return the parsed object
+ * @throws [JsonDataException] if the data does match the appropriate format
+ */
 @Throws
 inline fun <reified T : Any> Moshi.load(json: String): T {
     return this.adapter(T::class.java).fromJson(json)!!
 }
 
-//@Throws
-//inline fun <reified T : Any> Moshi.load(json: String, vararg types: Type): T {
-//    val adapterTypes = Types.newParameterizedType(T::class.java, *types)
-//    return this.adapter<T>(adapterTypes).fromJson(json)!!
-//}
+/**
+ * Load a List<T> from a JSON String.
+ * Example usage: moshi.load<List<Foo>>(json, Foo::class.java)
+ *
+ * @param json the json string
+ * @param types the type to parse the string into
+ * @return the parsed object
+ * @throws [JsonDataException] if the data does match the appropriate format
+ */
+@Throws
+inline fun <reified T : Any> Moshi.load(json: String, vararg types: Type): T {
+    val adapterTypes = Types.newParameterizedType(T::class.java, *types)
+    return this.adapter<T>(adapterTypes).fromJson(json)!!
+}
